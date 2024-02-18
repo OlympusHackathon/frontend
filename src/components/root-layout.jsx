fc7b60aad793148bfef8668b2a8f329598c09e9d
sabrina34352 <s.babakulova@student.inha.uz>
2024-02-18 14:17:31 +0500

    add history

--- src/components/root-layout.jsx
+++ src/components/root-layout.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SideBar } from "./side-bar";
 import { Outlet } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
 
 export const RootLayout = () => {
+  useEffect(() => {
+    localStorage.setItem("recentSummaries", JSON.stringify([]));
+  }, []);
   return (
     <Flex w="100%">
       <SideBar />
